2dbf7fb14e718bb8735d96e10fbccfd4f2e7f47e
kbunnyjoel <bunnyjoel391@gmail.com>
2025-04-27 03:52:48 +1000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,9 +5,9 @@ terraform {
       version = ">= 5.0"
     }
   }
-  required_version = "~> 1.10"
+  required_version = "> 1.5"
   backend "s3" {
-    bucket       = "your-s3-bucket-name"               # Replace with your bucket name
+    bucket       = "tfstate-s3-bucket-147997123166"
     key          = "terraform/state/terraform.tfstate" # Path to the state file
     region       = "us-east-1"
     encrypt      = true
