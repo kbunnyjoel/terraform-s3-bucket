baa06d80262edb5a682e99ab8764bdc59d19a7ba
kbunnyjoel <bunnyjoel391@gmail.com>
2025-04-27 03:51:38 +1000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,3 @@
-resource "aws_s3_bucket" "my_s3bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  tags = {
-    Name        = var.bucket_name
-    Environment = "dev"
-  }
-}
-
 output "bucket_name" {
   value = aws_s3_bucket.my_bucket.bucket
 }
